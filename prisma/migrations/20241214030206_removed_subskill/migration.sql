ec2857af2d6b2ed561a7230d3c348802d58024f7
Afeef <contact@afeef.dev>
2024-12-14 11:02:18 +0800

    feat: removed subskill in skills

---
+++ prisma/migrations/20241214030206_removed_subskill/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `subskill` on the `skills` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "skills" DROP COLUMN "subskill";
